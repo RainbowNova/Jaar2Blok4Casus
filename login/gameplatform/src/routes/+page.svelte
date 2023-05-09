43b63a19170a0801eead8aa3fe26212ce67dd554
Oscar Theelen <oscartheelen@gmail.com>
2023-05-09 19:02:53 +0200

    test

--- login/gameplatform/src/routes/+page.svelte
+++ login/gameplatform/src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <h1>Welcome to SvelteKit</h1>
-<h2>Hier komt een getal: {69}</h2>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href="/Test">klik voor test</a>
-
+<h2>Hier komt een getal: {number}</h2>
+<button on:click={incrementBtn}>Klik voor een hoger nummer</button>
 
 <script>
-    let number = 69;
+    let number = 0;
+    function incrementBtn(){
+        number += 1;
+    }
 </script>
