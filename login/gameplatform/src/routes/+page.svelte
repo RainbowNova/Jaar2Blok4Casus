356c0f55f4559a02529fb9f50e808acf7ca3144d
Oscar Theelen <oscartheelen@gmail.com>
2023-05-09 18:25:31 +0200

    test svelte

---
+++ login/gameplatform/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<h2>Hier komt een getal: {69}</h2>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/Test">klik voor test</a>
+
+
+<script>
+    let number = 69;
+</script>
