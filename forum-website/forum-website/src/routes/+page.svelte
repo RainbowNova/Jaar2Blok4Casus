e7c6eda6da4232c4ef398cbfc674012402048cee
Keano <keanoschoenmaeckers@outlook.com>
2023-05-11 15:54:19 +0200

    Basic skeleton website

---
+++ forum-website/forum-website/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    console.log("Hello!")
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/Categorie1">Categorie1</a>
+    <a href="/Categorie2">Categorie2</a>
+    <a href="/Categorie3">Categorie3</a>
+</nav>
+
+<h1>This is the home page of our forum, welcome!</h1>
