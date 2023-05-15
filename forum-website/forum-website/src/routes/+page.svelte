ff2fe071b856999faa838db3d33c158e18c10d44
Keano <keanoschoenmaeckers@outlook.com>
2023-05-15 12:08:07 +0200

    index html layout afgemaakt
    index html layout afgemaakt

--- forum-website/forum-website/src/routes/+page.svelte
+++ forum-website/forum-website/src/routes/+page.svelte
@@ -1,14 +1,38 @@
-<script lang="ts">
-
-</script>
-
 <nav>
     <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="/Categorie1">Categorie1</a></li>
-        <li><a href="/Categorie2">Categorie2</a></li>
-        <li><a href="/Categorie3">Categorie3</a></li>
+        <li><a href="/example-post-page">Example post page</a></li>
+        <li><a href="/example-topic-page">Example topic page</a></li>
     </ul>
 </nav>
 
 <h1>This is the home page of our forum, welcome!</h1>
+
+<textarea></textarea>
+<button>Plaats reactie</button>
+<div class="comments">
+    <div class="comment">
+        <div class="top-comment">
+            <p class="user">
+            </p>
+        </div>
+    </div>
+</div>
+
+<h1>Categorieën</h1>
+# Deze moeten uiteindelijk geautomatiseerd kunnen worden, zodat nieuwe categorieën toegevoegd kunnen worden.
+<div class="">
+    <h2>General/Off-topic</h2>
+    <div class="categories">
+        <div class="category">
+            
+        </div>
+    </div>
+    
+    <h2>Game Discussion</h2>
+    <div class="categories">
+        <div class="category">
+    
+        </div>
+    </div>
+</div>
---
+++ forum-website/forum-website/src/routes/example-post-page/+page.svelte
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/example-post-page">Example post page</a></li>
+        <li><a href="/example-topic-page">Example topic page</a></li>
+    </ul>
+</nav>
