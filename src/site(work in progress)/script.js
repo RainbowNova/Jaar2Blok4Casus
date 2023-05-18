886f87399daf12b7895a5af8079c41828068aade
Oscar Theelen <oscartheelen@gmail.com>
2023-05-18 16:15:49 +0200

    Working login page with starting page

--- src/site(work in progress)/script.js
+++ src/site(work in progress)/script.js
@@ -1,40 +1,42 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById('login-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        var username = sanitizeInput(document.getElementById('email').value);
+        var password = sanitizeInput(document.getElementById('password').value);
+        
+        console.log('Email:', username);
+        console.log('Password:', password);
+    });
+
+    function sanitizeInput(input) {
+        // replace potentally dangerous characters with html entity equivalents
+        var sanitizedInput = input
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#x27;')
+            .replace(/\//g, '&#x2F;');
+        
+        return sanitizedInput;
+    }
+
+    document.getElementById('register-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        var username = sanitizeInput(document.getElementById('register-username').value);
+        var email = sanitizeInput(document.getElementById('register-email').value);
+        var password = sanitizeInput(document.getElementById('register-password').value);
     
-    var username = sanitizeInput(document.getElementById('email').value);
-    var password = sanitizeInput(document.getElementById('password').value);
-    
-    console.log('Email:', username);
-    console.log('Password:', password);
+        //var address = sanitizeInput(document.getElementById('address').value);
+        //var dateOfBirth = sanitizeInput(document.getElementById('date-of-birth').value);
+        
+        console.log('Register-Username:', username);
+        console.log('Register-Email:', email);
+        console.log('Register-Password:', password);
+        //console.log('Address:', address);
+        //console.log('Date of Birth:', dateOfBirth);
+    });
 });
-
-function sanitizeInput(input) {
-    // replace potentally dangerous characters with html entity equivalents
-    var sanitizedInput = input
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#x27;')
-        .replace(/\//g, '&#x2F;');
-    
-    return sanitizedInput;
-}
-
-document.getElementById('register-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    var username = sanitizeInput(document.getElementById('register-username').value);
-    var email = sanitizeInput(document.getElementById('register-email').value);
-    var password = sanitizeInput(document.getElementById('register-password').value);
-  
-    //var address = sanitizeInput(document.getElementById('address').value);
-    //var dateOfBirth = sanitizeInput(document.getElementById('date-of-birth').value);
-    
-    console.log('Register-Username:', username);
-    console.log('Register-Email:', email);
-    console.log('Register-Password:', password);
-    //console.log('Address:', address);
-    //console.log('Date of Birth:', dateOfBirth);
-});
-
--- src/site(work in progress)/script2.js
+++ src/site(work in progress)/script2.js
@@ -1,12 +1,26 @@
-//switch between register and login
-wrapper = document.querySelector('.wrapper');
-loginLink = document.querySelector('.login-link');
-registerLink = document.querySelector('.register-link');
+document.addEventListener('DOMContentLoaded', () => {
+   
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.login-link');
+    const registerLink = document.querySelector('.register-link');
+    const btnPopup = document.querySelector('.btnLogin-popup');
+    const iconClose = document.querySelector('.icon-close');
+  
 
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-})
+    registerLink.addEventListener('click', () => {
+      wrapper.classList.add('active');
+    });
+  
+    loginLink.addEventListener('click', () => {
+      wrapper.classList.remove('active');
+    });
+  
+    btnPopup.addEventListener('click', () => {
+      wrapper.classList.add('active-popup');
+    });
 
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-})
+    iconClose.addEventListener('click', () => {
+        wrapper.classList.remove('active-popup');
+      });
+});
+  
