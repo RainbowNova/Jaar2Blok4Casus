4e7a3035a5baf1fb743890579385fd60a35e922b
Oscar Theelen <oscartheelen@gmail.com>
2023-05-18 12:32:51 +0200

    Input safety added

--- src/pages/login/script.js
+++ src/pages/login/script.js
@@ -1,10 +1,22 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+    var username = sanitizeInput(document.getElementById('username').value);
+    var password = sanitizeInput(document.getElementById('password').value);
     
     console.log('Username:', username);
     console.log('Password:', password);
 });
-  
+
+function sanitizeInput(input) {
+    // replace potentally dangerous characters with html entity equivalents
+    var sanitizedInput = input
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#x27;')
+        .replace(/\//g, '&#x2F;');
+    
+    return sanitizedInput;
+}
--- src/pages/register/script.js
+++ src/pages/register/script.js
@@ -1,15 +1,28 @@
 document.getElementById('register-form').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var email = document.getElementById('email').value;
-    var address = document.getElementById('address').value;
-    var dateOfBirth = document.getElementById('date-of-birth').value;
+    var username = sanitizeInput(document.getElementById('username').value);
+    var password = sanitizeInput(document.getElementById('password').value);
+    var email = sanitizeInput(document.getElementById('email').value);
+    var address = sanitizeInput(document.getElementById('address').value);
+    var dateOfBirth = sanitizeInput(document.getElementById('date-of-birth').value);
     
     console.log('Username:', username);
     console.log('Password:', password);
     console.log('Email:', email);
     console.log('Address:', address);
     console.log('Date of Birth:', dateOfBirth);
-  });
+});
+
+function sanitizeInput(input) {
+  // replace potentally dangerous characters with html entity equivalents
+  var sanitizedInput = input
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#x27;')
+      .replace(/\//g, '&#x2F;');
+  
+  return sanitizedInput;
+}
