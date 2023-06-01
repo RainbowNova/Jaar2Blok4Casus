2449391e818d31e387164f2145e338e509feb307
Oscar Theelen <oscartheelen@gmail.com>
2023-06-01 13:12:19 +0200

    added folder for firebase

--- firebase/register.js
+++ firebase/register.js
@@ -1,5 +1,3 @@
-
-
 const firebaseConfig = {
     apiKey: "AIzaSyBlxNS6DfDzWIHeksRpE8brSO0odUQrbwM",
     authDomain: "gameplatform-db.firebaseapp.com",
@@ -14,28 +12,10 @@ const firebaseConfig = {
 //initialize firebase
 firebase.initializeApp(firebaseConfig);
 
-
-//create firebase database reference LOGIN
-/*var contactFormDB = firebase.database().ref("login-form")
-
-document.getElementById("login-form").addEventListener("submit", submitForm);
-
-function submitForm(e) {
-    e.preventDefault();
-
-    var email = getElementVal("email");
-    var password = getElementVal("password");
-}
-
-const getElementVal = (id) => {
-    return document.getElementById(id).value;
-}
-*/
-
-//create firebase database reference REGISTER
+//Register account into accounts table
 
 document.addEventListener('DOMContentLoaded', () => {
-    var registerFormDB = firebase.database().ref("register-form")
+    var registerFormDB = firebase.database().ref("accounts")
 
     document.getElementById("register-form").addEventListener("submit", submitForm);
 
@@ -48,8 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         var address = getElementVal("register-Address");
         var birthday = getElementVal("register-Date of Birth");
         var password = getElementVal("register-password");
-
-        console.log(username, email, location, address, birthday, password);
+  
 
         storeAccount(username, email, location, address, birthday, password);
     }
@@ -70,4 +49,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const getElementVal = (id) => {
         return document.getElementById(id).value;
     }
+
 });
+
