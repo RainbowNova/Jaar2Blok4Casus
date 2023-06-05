f137241bbfa49fd205d3964f0d454aad5ccd2e56
Oscar Theelen <oscartheelen@gmail.com>
2023-06-05 13:50:15 +0200

    added login function

---
+++ firebase/login.js
@@ -0,0 +1,46 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlxNS6DfDzWIHeksRpE8brSO0odUQrbwM",
+    authDomain: "gameplatform-db.firebaseapp.com",
+    databaseURL: "https://gameplatform-db-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "gameplatform-db",
+    storageBucket: "gameplatform-db.appspot.com",
+    messagingSenderId: "545500280718",
+    appId: "1:545500280718:web:95e285f583b0157adf56df",
+    measurementId: "G-R2NMTXLKF5"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+
+//ADD AUTHENTICATION, LOGIN.... THEN ADD TEMPORARY JS STORAGE FOR LOGIN SESSION STORAGE
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById("login-form").addEventListener("submit", tryLogin);
+
+    function tryLogin(e) {
+        firebase.database().ref("accounts").once("value")
+        .then((snapshot) => {
+
+            const usersData = snapshot.val();
+
+
+            for (const userId in usersData) {
+            if (usersData.hasOwnProperty(userId)) {
+                const user = usersData[userId];
+
+                const email = user.email;
+                const password = user.password;
+
+                console.log("Email: ", email);
+                console.log("Password: ", password);
+            }
+            }
+        })
+        .catch((error) => {
+            console.log("Error reading data: ", error);
+        });
+    }	
+});
