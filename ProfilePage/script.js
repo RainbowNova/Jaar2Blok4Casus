58b04118e1d1605bc481d314ed3ddac165ff2512
Thomas Palmen <121097637+thmaz@users.noreply.github.com>
2023-06-13 09:21:15 +0200

    localstorage points

--- ProfilePage/script.js
+++ ProfilePage/script.js
@@ -1,2 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-});
+document.addEventListener("DOMContentLoaded", function () {
+    var points = localStorage.getItem("points");
+    if (points !== null) {
+      var level = Math.floor(points / 100) + 1;
+      document.getElementById("points").textContent = "Points: " + points;
+      document.getElementById("level").textContent = "Level: " + level;
+    }
+  });
