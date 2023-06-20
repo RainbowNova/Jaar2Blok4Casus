2ce315a3da6ed212495f0d8e7c7c16b1634068fd
Oscar Theelen <oscartheelen@gmail.com>
2023-06-20 14:52:56 +0200

    replaced bootstrap 4.0.0 with 5.3.0

--- components/nicer_header.js
+++ components/nicer_header.js
@@ -6,32 +6,36 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-custom fixed-top" style="background-color: rgba(48, 41, 144, 0.514);">
-                <a class="navbar-brand logo" href="../index.html">Damp</a>
+         
+            <nav class="navbar navbar-expand-lg navbar-light bg-custom fixed-top" style="background-color: rgba(48, 41, 144, 0.514);">  
+                <a class="navbar-brand logo" href="../index.html" style="margin-left:10px;">Damp</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse navigation" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item" >
-                            <a class="nav-link" href="../index.html" style="color: white;">Home</a>
-                        </li>
-                        <li class="nav-item" >
-                            <a class="nav-link" href="../ProfilePage/Profile.html" style="color: white;">Profile</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../StorePage/StorePage.html" style="color: white;">Store</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../ForumPage/Forum.html" style="color: white;">Forum</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../AboutPage/About.html" style="color: white;">About</a>
-                        </li>
-                    </ul>
-                    <button id="loginButton" class="btn btnLogin-popup btn-outline-light">Login</button>
-                </div>
+
+                    <div class="collapse navbar-collapse navigation" id="navbarNav" style="justify-content: end; margin-right: 10px;">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item" >
+                                <a class="nav-link" href="../index.html" style="color: white;">Home</a>
+                            </li>
+                            <li class="nav-item" >
+                                <a class="nav-link" href="../ProfilePage/Profile.html" style="color: white;">Profile</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="../StorePage/StorePage.html" style="color: white;">Store</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="../ForumPage/Forum.html" style="color: white;">Forum</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="../AboutPage/About.html" style="color: white;">About</a>
+                            </li>
+                        </ul>
+                        <button id="loginButton" class="btn btnLogin-popup btn-outline-light">Login</button>
+                    </div>
+ 
             </nav>
+    
         </header>
 
     
