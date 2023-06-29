a209dc7ffe251f570a76e8b8f0e005fb0d3f6d2b
Thomas Palmen <121097637+thmaz@users.noreply.github.com>
2023-06-29 19:06:21 +0200

    removed unused code

--- Game1Page/snake.js
+++ Game1Page/snake.js
@@ -1,23 +1,3 @@
-/*import { initializeApp } from "firebase/app";
-import{
-    getFireStore, collection, onSnapShot,
-    query, addDoc, doc, where, orderBy, limit,
-    getDoc, updateDoc, getDocs, setDoc
-} from 'firebase/firestore';
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCGi7d3hQFtXooGw_HosUqGrH0dBJYojYI",
-    authDomain: "damp-c7a9d.firebaseapp.com",
-    projectId: "damp-c7a9d",
-    storageBucket: "damp-c7a9d.appspot.com",
-    messagingSenderId: "1045743798558",
-    appId: "1:1045743798558:web:8ac8bd44114553396ee10f"
-  };
-
-initializeApp(firebaseConfig);
-
-const db = getFireStore(); */
-
 // Variables
 // Board for snake
 var blockSize= 25;
