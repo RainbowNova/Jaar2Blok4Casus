8a6d965d4eba75e580d94737b562040b8f8edf8f
Jasper <smitzjasper@gmail.com>
2023-06-29 20:14:50 +0200

    Fixed categories?

--- ForumPage/LoadCategories.js
+++ ForumPage/LoadCategories.js
@@ -1,89 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    createCategories();
-    displayCategories();
-  });
-
-  // Function to fetch game data from the API
-async function fetchGameData() {
-    try {
-      const response = await fetch('https://dampbackendapi.azurewebsites.net/api/Games');
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(`Failed to fetch game data: ${error}`);
-    }
-  }
-  
-  // Function to create categories based on game data
-  async function createCategories() {
-    try {
-      const gameData = await fetchGameData();
-      const categories = gameData.map((game) => ({
-        title: game.name,
-        description: game.description,
-        gameId: game.id,
-      }));
-  
-      // Send a POST request to create categories in the API
-      const response = await fetch('https://dampbackendapi.azurewebsites.net/api/Categories', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(categories),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-      console.log('Categories created successfully');
-    } catch (error) {
-      console.error(`Failed to create categories: ${error}`);
-    }
-  }
-  
-  // Function to fetch categories from the API
-  async function fetchCategories() {
+// Function to fetch categories from the API
+async function fetchCategories() {
     try {
       const response = await fetch('https://dampbackendapi.azurewebsites.net/api/Categories');
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
       const data = await response.json();
       return data;
     } catch (error) {
-      console.error(`Failed to fetch categories: ${error}`);
+      console.error('Error fetching categories:', error);
+      return [];
     }
   }
   
-  // Function to display categories on the forum page
-  async function displayCategories() {
-    try {
-      const categories = await fetchCategories();
-      const forumContainer = document.getElementById('forum-container');
+  // Function to create category elements dynamically
+  function createCategoryElements(categories) {
+    const categoryContainer = document.getElementById('category-container'); // Replace 'category-container' with the ID of the container element where you want to display the categories
   
-      // Clear existing content in the forum container
-      forumContainer.innerHTML = '';
+    categories.forEach((category) => {
+      const categoryElement = document.createElement('div');
+      categoryElement.classList.add('category');
+      categoryElement.innerHTML = `
+        <h2>${category.title}</h2>
+        <p>${category.description}</p>
+      `;
   
-      // Create and append category elements to the forum container
-      categories.forEach((category) => {
-        const categoryElement = document.createElement('div');
-        categoryElement.innerHTML = `
-          <h2>${category.title}</h2>
-          <p>${category.description}</p>
-          <p>Game ID: ${category.gameId}</p>
-        `;
-        forumContainer.appendChild(categoryElement);
-      });
-    } catch (error) {
-      console.error(`Failed to display categories: ${error}`);
-    }
+      categoryContainer.appendChild(categoryElement);
+    });
   }
   
-  // Call the functions to create categories and display them on the forum page
-  createCategories();
-  displayCategories();
+  // Fetch categories and create category elements when the page loads
+  window.addEventListener('load', async () => {
+    const categories = await fetchCategories();
+    createCategoryElements(categories);
+  });
   
